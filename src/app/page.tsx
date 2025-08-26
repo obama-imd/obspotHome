c1fba0726df20a2af7eb7b7d009f1776f7958d6f
Emanuel Kywal <emanuelkywal@outlook.com>
2025-08-26 15:52:41 -0300

    feat: basic footer component file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,36 @@
 import Header from "@/components/common/header/basicHeader";
 import Main from "@/components/landing-page/main/landingPageMain";
+import Footer from "@/components/common/footer/footer";
 
 export default function Home() {
   return (
-    <div className="
-      min-h-600
-      flex flex-col gap-30  
-      bg-background-secondary
-    ">
-      <header className="
-        flex items-start justify-center align-spa
-      ">
+    <div 
+      className="
+        flex flex-col gap-y-25  
+        bg-background-secondary
+      "
+    >
+      
+      <header 
+        className="
+          flex items-start justify-center align-spa
+        "
+      >
         <Header/>
       </header>
-      <main className="
-        h-400
-      ">
+      
+      <main>
         <Main/>
       </main> 
-      <footer className="">
+      
+      <footer 
+        className="
+          w-full h-auto
+        "
+      >
+        <Footer/>
       </footer>
+
     </div>
   );
 }
---
+++ src/components/common/footer/footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Footer = () => {
+
+    return (
+        <div
+            className="
+                w-full h-auto
+                bg-black-500
+            "
+        >
+            add footer here
+        </div>
+    )
+}
+
+export default Footer;
