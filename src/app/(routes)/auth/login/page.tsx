90922d2f5a5395dced2ac1eead33d34f14fd564e
denisRdSilva <denisrsilva95@gmail.com>
2025-09-10 01:53:26 -0300

    Configuração dos endpoints via axios

--- src/app/(routes)/auth/login/page.tsx
+++ src/app/(routes)/auth/login/page.tsx
@@ -1,24 +1,43 @@
 "use client"
 import { useState } from "react"
 import "../auth.css"
+import api from '../../../../utils/axios';
+import Header from "@/components/common/header/basicHeader";
+import Footer from "@/components/common/footer/footer";
 
 export default function LoginPage() {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
-    const apiURL = "/auth";
-
+    function signIn() {
+        api.post('/auth/login', {
+            email,
+            password
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
     return (
-        <div className="auth">
-            <div className="card flex column">
-                <label>E-mail</label>
-                <input className="inputBox" type="text" value={email} onChange={(e) => { setEmail(e.target.value) }} />
-                <label>Senha</label>
-                <input className="inputBox" type="text" value={password} onChange={(e) => { setPassword(e.target.value) }} />
-                <button className="btn">Entrar</button>
-                <a className="link" href="./register">Cadastrar</a>
+        <>
+            <Header />
+            <div className="auth">
+                <div className="card flex column">
+                    <label>E-mail</label>
+                    <input className="inputBox" type="text" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                    <label>Senha</label>
+                    <input className="inputBox" type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                    <button className="btn" onClick={signIn}>Entrar</button>
+                    <a className="link" href="./register">Cadastrar</a>
+                </div>
             </div>
-        </div>
+            <Footer />
+
+        </>
+
     )
 }
--- src/app/(routes)/auth/register/page.tsx
+++ src/app/(routes)/auth/register/page.tsx
@@ -1,6 +1,10 @@
 "use client"
 import { useState } from "react"
 import "../auth.css"
+import api from '../../../../utils/axios';
+import Header from "@/components/common/header/basicHeader";
+import Footer from "@/components/common/footer/footer";
+
 
 export default function LoginPage() {
     const [email, setEmail] = useState<string>("");
@@ -8,22 +12,49 @@ export default function LoginPage() {
     const [confirmPassword, setConfirmPassword] = useState<string>("");
     const [name, setName] = useState<string>("");
     const [lastName, setLastName] = useState<string>("");
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+
+    function signUp() {
+        api.post('/auth/register', {
+            nome: name,
+            sobrenome: lastName,
+            email,
+            password,
+            confirmPassword
+        })
+            .then(function (response) {
+                const token = response.data.token;
+                localStorage.setItem('token', token);
+                // console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible);
+    }
 
     return (
-        <div className="auth">
-            <div className="card flex">
-                <label>Nome</label>
-                <input className="inputBox" type="text" value={name} onChange={(e) => { setName(e.target.value) }} />
-                <label>Sobrenome</label>
-                <input className="inputBox" type="text" value={lastName} onChange={(e) => { setLastName(e.target.value) }} />
-                <label>E-mail</label>
-                <input className="inputBox" type="text" value={email} onChange={(e) => { setEmail(e.target.value) }} />
-                <label>Senha</label>
-                <input className="inputBox" type="text" value={password} onChange={(e) => { setPassword(e.target.value) }} />
-                <label>Confirmar Senha</label>
-                <input className="inputBox" type="text" value={confirmPassword} onChange={(e) => { setConfirmPassword(e.target.value) }} />
-                <button className="btn">Cadastrar</button>
+        <>
+            <Header />
+            <div className="auth">
+                <div className="card flex">
+                    <label>Nome</label>
+                    <input className="inputBox" type="text" value={name} onChange={(e) => { setName(e.target.value) }} />
+                    <label>Sobrenome</label>
+                    <input className="inputBox" type="text" value={lastName} onChange={(e) => { setLastName(e.target.value) }} />
+                    <label>E-mail</label>
+                    <input className="inputBox" type="text" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                    <label>Senha</label>
+                    <input className="inputBox" type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                    <label>Confirmar Senha</label>
+                    <input className="inputBox" type="password" value={confirmPassword} onChange={(e) => { setConfirmPassword(e.target.value) }} />
+                    <button className="btn" onClick={signUp}>Cadastrar</button>
+                </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
